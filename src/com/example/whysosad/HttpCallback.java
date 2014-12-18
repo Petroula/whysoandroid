cb84a0825fd34d5d617afcc50efdadf46228eb32
davidkron <david_jensen3@hotmail.com>
2014-12-18 22:11:53 +0100

    removed old package

--- src/com/example/whysosad/HttpCallback.java
+++
@@ -1,5 +0,0 @@
-package com.example.whysosad;
-
-public interface HttpCallback {
-	abstract void callback(String response);
-}
--- src/com/example/whysosad/User.java
+++
@@ -1,6 +0,0 @@
-package com.example.whysosad;
-
-public class User {
-static String username = "";
-static String password = "";
-}
