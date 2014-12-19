f1f882a4856815e0dad1119fc6be702a9e4b4221
petroula <gustheodpe@student.gu.se>
2014-12-19 21:17:21 +0100

    fixes

--- src/pegasus/Whysosad/AboutActivity.java
+++ src/pegasus/Whysosad/AboutActivity.java
@@ -1,14 +1,23 @@
 package pegasus.Whysosad;
 import pegasus.Whysosad.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends Activity implements OnClickListener {
+	
+	Button back;
 	
 	protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.about_page);
+    	
+    	back = (Button)findViewById(R.id.back);
+    	back.setOnClickListener(this);
     	
     	String totalUsers = ClientToServer.totalUsers();
     	TextView aboutApp = (TextView)findViewById(R.id.aboutApp);
@@ -17,5 +26,13 @@ public class AboutActivity extends Activity {
     			+ "credits on a country's score.The user has to register/login as soon as the application starts and is "
     			+ "allowed to go back and forth between most of the pages.Currently we have a total of " + totalUsers +
     			" registered users.");
-    }	
+    }
+
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == R.id.back) {
+			Intent change = new Intent(getApplicationContext(), WebActivity.class);
+			startActivity(change);
+		}
+	}	
 }
